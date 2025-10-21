c47d9a4f0b194925e06507868b3c54997ada062c
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-10-21 14:59:44 +0200

    add Calculator Class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+namespace Example
+{
+    public class Calculator
+    {
+        // Adds two integer numbers and returns their sum
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result1 = calc.AddNumbers(5, 3);
+
+            Console.WriteLine($"AddNumbers result: {result1}");
+        }
+    }
+}
